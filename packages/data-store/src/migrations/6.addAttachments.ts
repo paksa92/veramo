85cf653f3eea3a6766b538a8535b1545f0808cc2
Sharif Paksa <s.paksa@protonmail.com>
2025-07-27 00:25:29 +0200

    fix: why it no work

--- packages/data-store/src/migrations/6.addAttachments.ts
+++ packages/data-store/src/migrations/6.addAttachments.ts
@@ -10,6 +10,14 @@ export class AddAttachments1753543810823 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         debug(`Running migration 'AddAttachments1753543810823'`)
 
+        if (queryRunner.connection.driver.options.type === 'sqlite') {
+            debug(`splitting migration into multiple transactions to allow sqlite table updates`)
+            await queryRunner.commitTransaction()
+            debug(`turning off foreign keys`)
+            await queryRunner.query('PRAGMA foreign_keys=off')
+            await queryRunner.startTransaction()
+        }
+
         debug(`adding 'attachments' column to 'message' table`)
         await queryRunner.addColumn(migrationGetExistingTableByName(queryRunner, 'message', true), new TableColumn({
             name: "attachments",
@@ -18,15 +26,40 @@ export class AddAttachments1753543810823 implements MigrationInterface {
         }))
         debug(`added 'attachments' column to 'message' table`)
 
+        if (queryRunner.connection.driver.options.type === 'sqlite') {
+            debug(`splitting migration into multiple transactions to allow sqlite table updates`)
+            await queryRunner.commitTransaction()
+            debug(`turning on foreign keys`)
+            await queryRunner.query('PRAGMA foreign_keys=on')
+            await queryRunner.startTransaction()
+        }
+
         debug(`Migration 'AddAttachments1753543810823' completed successfully`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         debug(`Rolling back migration 'AddAttachments1753543810823'`)
 
+        if (queryRunner.connection.driver.options.type === 'sqlite') {
+            debug(`splitting migration into multiple transactions to allow sqlite table updates`)
+            await queryRunner.commitTransaction()
+            debug(`turning off foreign keys`)
+            await queryRunner.query('PRAGMA foreign_keys=off')
+            await queryRunner.startTransaction()
+        }
+
+
         debug(`removing 'attachments' column from 'message' table`)
         await queryRunner.dropColumn(migrationGetExistingTableByName(queryRunner, 'message', true), 'attachments')
         debug(`removed 'attachments' column from 'message' table`)
+
+        if (queryRunner.connection.driver.options.type === 'sqlite') {
+            debug(`splitting migration into multiple transactions to allow sqlite table updates`)
+            await queryRunner.commitTransaction()
+            debug(`turning on foreign keys`)
+            await queryRunner.query('PRAGMA foreign_keys=on')
+            await queryRunner.startTransaction()
+        }
 
         debug(`Migration 'AddAttachments1753543810823' rolled back successfully`)
     }
